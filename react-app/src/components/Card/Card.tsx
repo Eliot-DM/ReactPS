3e35ab8ee3b555f77bb63f3466edb65ca5cf8c26
Dmitriy- <malyuchenko.d96@mail.ru>
2026-07-24 22:10:29 +0700

    16

--- react-app/src/components/Card/Card.tsx
+++ react-app/src/components/Card/Card.tsx
@@ -1,62 +1,39 @@
-import {
-  addFavorite,
-  deleteFavorite,
-  Movie,
-} from "../../store/favorites.slice";
 import styles from "./Card.module.css";
-import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "../../store/store"; // импортируем тип
+import { Link } from "react-router-dom";
 
 interface CardProps {
   title: string;
-  src: string;
   star: string;
+  src: string;
+  isFavorite?: boolean; // Опциональный пропс
+  onToggleFavorite?: () => void; // Опциональный пропс
 }
 
-export const Card = ({ title, src, star }: CardProps) => {
-  const dispatch = useDispatch();
-  // Заменяем any на RootState
-  const favorites = useSelector((state: RootState) => state.favorites.favorite);
-
-  const isFavorite = favorites.some((movie: Movie) => movie.title === title);
-
-  const handleToggleFavorite = () => {
-    const movie: Movie = { title, src, star };
-
-    if (isFavorite) {
-      dispatch(deleteFavorite(title));
-    } else {
-      dispatch(addFavorite(movie));
-    }
-  };
-
+export const Card = ({
+  title,
+  star,
+  src,
+  isFavorite = false,
+  onToggleFavorite,
+}: CardProps) => {
   return (
     <li className={styles.card}>
-      <div className={styles.star}>
-        <img className={styles.star_svg} src="/public/svg/star.svg" alt="" />
-        <p className={styles.star_text}>{star}</p>
-      </div>
-      <img className={styles.img} src={src} alt={title} />
-      <p className={styles.card_title}>{title}</p>
-
-      <div
-        className={styles.block_link}
-        onClick={handleToggleFavorite}
-        style={{ cursor: "pointer" }}
-        role="button"
-        tabIndex={0}
-      >
-        <img
-          className={styles.like_svg}
-          src={
-            isFavorite ? "/public/svg/like-active.svg" : "/public/svg/like.svg"
-          }
-          alt={isFavorite ? "Убрать из избранного" : "Добавить в избранное"}
-        />
-        <span className={styles.link}>
-          {isFavorite ? "В избранном" : "В избранное"}
-        </span>
-      </div>
+      <Link to={`/movie/${title}`}>
+        <img src={src} alt={title} className={styles.image} />
+        <h3 className={styles.title}>{title}</h3>
+        <p className={styles.star}>⭐ {star}</p>
+      </Link>
+      {onToggleFavorite && (
+        <button
+          className={`${styles.favoriteButton} ${isFavorite ? styles.active : ""}`}
+          onClick={(e) => {
+            e.preventDefault();
+            onToggleFavorite();
+          }}
+        >
+          {isFavorite ? "❤️" : "🤍"}
+        </button>
+      )}
     </li>
   );
 };
